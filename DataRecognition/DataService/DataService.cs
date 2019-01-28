d07786999e6e58d8b833f8a2633c75761409d044
Michael H <hamhoev-m@mail.ru>
2019-01-28 17:55:45 +0700

    created branch

---
+++ DataRecognition/DataService/DataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace DataService
+{
+    /// <summary>
+    /// Среда выполнения Service Fabric создает экземпляр этого класса для каждого экземпляра службы.
+    /// </summary>
+    internal sealed class DataService : StatelessService
+    {
+        public DataService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Необязательное переопределение для создания прослушивателей (например, TCP, HTTP) для этой реплики службы, чтобы обрабатывать запросы клиентов или пользователей.
+        /// </summary>
+        /// <returns>Коллекция прослушивателей.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[0];
+        }
+
+        /// <summary>
+        /// Это главная точка входа для экземпляра вашей службы.
+        /// </summary>
+        /// <param name="cancellationToken">Отменяется, когда Service Fabric нужно завершить работу этого экземпляра службы.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: замените приведенный ниже пример кода на свою логику 
+            //       или удалите это переопределение RunAsync, если оно не нужно для вашей службы.
+
+            long iterations = 0;
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
---
+++ DataRecognition/DataService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace DataService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Это точка входа для хост-процесса службы.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                // В файле ServiceManifest.XML определяется одно имя типа служб или несколько.
+                // При регистрации службы имя ее типа сопоставляется с типом .NET.
+                // Когда Service Fabric создает экземпляр этого типа службы,
+                // в этом хост-процессе создается экземпляр класса.
+
+                ServiceRuntime.RegisterServiceAsync("DataServiceType",
+                    context => new DataService(context)).GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(DataService).Name);
+
+                // Предотвращает завершение работы этого хост-процесса, обеспечивая непрерывную работу служб.
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
